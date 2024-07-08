4e989e264ce831075f868c3da78883fe275af0af
Shivam4160 <sq.shivam4106@gmail.com>
2024-07-08 10:59:39 +0530

    6-7-20243

--- C-D-over.cpp
+++ C-D-over.cpp
@@ -6,10 +6,10 @@ class volume
 private:
     
 public:
-    volume()
+    volume( int a)
     {
 
-        int a;
+       
         float cube;
         cube=a*a*a;
         cout<<"volume of cube is"<<":"<<cube<<endl;
@@ -22,37 +22,49 @@ public:
         
         float cone;
         cone= 3.14*r*r*h/3;
-        cout<<"voume of cube is"<<":"<<cone<<endl;
+        cout<<"voume of cone is"<<":"<<cone<<endl;
 
 
     }
-    volume(int a)
+    volume(int l,int b,int h)
     {
 
-        int l,b,h;
+       
         float cuboid;
         cuboid=l*b*h;
         cout<<"volume of cuboid is"<<":"<<cuboid<<endl;
 
 
     }
-    volume()
+    volume(float r)
     {
 
-        int a,volume;
-        volume=a*a*a;
-        cout<<"volume of cube is"<<":"<<volume<<endl;
+        float sphere;
+        sphere=(4/3)*3.14*r*r*r;
+        cout<<"volume of sphere is"<<":"<<sphere<<endl;
 
 
     }
-    volume()
-    {
-
-        int a,volume;
-        volume=a*a*a;
-        cout<<"volume of cube is"<<":"<<volume<<endl;
-
-
-    }
+    
 
 };
+
+int main()
+{
+
+    int n;
+
+    printf("cube\tcone\tcuboid\tsphere\n");
+    printf("1\t2\t3\t4\n");
+    printf("enter the number according to the shape of object you want to find volume:");
+    scanf("%d",&n);
+
+    if (n==1)
+    {
+        
+    }
+    
+
+    return 0;
+}
+
