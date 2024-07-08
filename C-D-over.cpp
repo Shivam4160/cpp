56de14dff6efb20a0ede7bb5b44b9b6bc76f9b80
shivam tejani <sqshivam4106@gmail.com>
2024-07-08 18:50:23 +0530

    8-7-2024

--- C-D-over.cpp
+++ C-D-over.cpp
@@ -1,58 +1,71 @@
-#include<iostream>
-using namespace std;
-
-class volume
-{
-private:
-    
-public:
-    volume()
-    {
-
-        int a;
-        float cube;
-        cube=a*a*a;
-        cout<<"volume of cube is"<<":"<<cube<<endl;
-
-
-    }
-    volume(int r, int h)
-    {
-
+#include<iostream> 
+using namespace std; 
+ 
+class volume 
+{ 
+private: 
+     
+public: 
+    volume( int a) 
+    { 
+ 
         
-        float cone;
-        cone= 3.14*r*r*h/3;
-        cout<<"voume of cube is"<<":"<<cone<<endl;
+        float cube; 
+        cube=a*a*a; 
+        cout<<"volume of cube is"<<":"<<cube<<endl; 
+ 
+ 
+    } 
+    volume(int r, int h) 
+    { 
+ 
+         
+        float cone; 
+        cone= 3.14*r*r*h/3; 
+        cout<<"voume of cone is"<<":"<<cone<<endl; 
+ 
+ 
+    } 
+    volume(int l,int b,int h) 
+    { 
+ 
+        
+        float cuboid; 
+        cuboid=l*b*h; 
+        cout<<"volume of cuboid is"<<":"<<cuboid<<endl; 
+ 
+ 
+    } 
+    volume(float r) 
+    { 
+ 
+        float sphere; 
+        sphere=(4/3)*3.14*r*r*r; 
+        cout<<"volume of sphere is"<<":"<<sphere<<endl; 
+ 
+ 
+    } 
+     
+ 
+}; 
+ 
+int main() 
+{ 
+ 
+    int n; 
+ 
+    cout<<"cube\tcone\tcuboid\tsphere"<<endl; 
+    cout<<"1\t2\t3\t4"<<endl;
+    cout<<"enter the number according to the shape of object you want to find volume:"; 
+    scanf("%d",&n); 
+ 
+    if (n==1) 
+    { 
+         
+        
 
-
-    }
-    volume(int a)
-    {
-
-        int l,b,h;
-        float cuboid;
-        cuboid=l*b*h;
-        cout<<"volume of cuboid is"<<":"<<cuboid<<endl;
-
-
-    }
-    volume()
-    {
-
-        int a,volume;
-        volume=a*a*a;
-        cout<<"volume of cube is"<<":"<<volume<<endl;
-
-
-    }
-    volume()
-    {
-
-        int a,volume;
-        volume=a*a*a;
-        cout<<"volume of cube is"<<":"<<volume<<endl;
-
-
-    }
-
-};
+    } 
+     
+ 
+    return 0; 
+}
