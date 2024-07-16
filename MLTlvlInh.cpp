cb9d6250ed2c5edd944731f8ac1857d8f4de3377
shivam tejani <sqshivam4106@gmail.com>
2024-07-16 18:53:58 +0530

    16-7-2024

--- MLTlvlInh.cpp
+++ MLTlvlInh.cpp
@@ -1,75 +1,60 @@
 #include <iostream>
 using namespace std;
 
-class batsman 
+class batsman
 {
     float calcavg();
-    
-    protected:
+
+protected:
     int bcode;
     char bname[20];
     int innings, notout, runs;
-    float bavg ;
+    float bavg;
 
-    public:
-
-    
-
-   
-
+public:
     void displaydata()
     {
 
-        cout<<"name of batsman is:"<<bname<<endl;
-        cout<<"bcode of batsname is:"<<bcode<<endl;
-        cout<<"innings of batsname is:"<<innings<<endl;
-        cout<<"notout:"<<notout<<endl;
-        cout<<"runs of batsname is:"<<runs<<endl;
-        cout<<"avg of batsname is"<<calcavg();
-
+        cout << "name of batsman is:" << bname << endl;
+        cout << "bcode of batsname is:" << bcode << endl;
+        cout << "innings of batsname is:" << innings << endl;
+        cout << "notout:" << notout << endl;
+        cout << "runs of batsname is:" << runs << endl;
+        cout << "avg of batsname is" << calcavg();
     }
-
-   
-    
 };
-class readdata1:public batsman 
+class readdata1 : public batsman
 {
-    public:
+public:
     void readdata()
     {
 
-        cout<<"enter bcode of batsman:"<<endl;
-        cin>>bcode;
-        cout<<"enter the name of batsman:"<<endl;
-        cin>>bname;
-        cout<<"enter innings of batsman:"<<endl;
-        cin>>innings;
-        cout<<"enter the value of notout:"<<endl;
-        cin>>notout;
-        cout<<"enter runs of batsman:"<<endl;
-        cin>>runs;
-
+        cout << "enter bcode of batsman:" << endl;
+        cin >> bcode;
+        cout << "enter the name of batsman:" << endl;
+        cin >> bname;
+        cout << "enter innings of batsman:" << endl;
+        cin >> innings;
+        cout << "enter the value of notout:" << endl;
+        cin >> notout;
+        cout << "enter runs of batsman:" << endl;
+        cin >> runs;
     }
-
-
-
-
 };
 
 int main()
 {
-    
+
     class readdata1 obj;
 
     obj.readdata();
     obj.displaydata();
 
-    
     return 0;
 }
 
 float batsman::calcavg()
-    {
-        bavg=(float)runs/(innings-notout);
-        return (float)bavg;
-    }
+{
+    bavg = (float)runs / (innings - notout);
+    return (float)bavg;
+}
